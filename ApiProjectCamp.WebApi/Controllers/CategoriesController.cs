f57ab33ab6f9ea26120b4e9c361baaaed88c1db1
Murat Yıldırım <30393559+murat-yildirim@users.noreply.github.com>
2025-02-10 22:32:17 +0300

    6-7-8.Ders sonu Chef ve Categori Controller eklendi , dependency İnjection uygulandı Api ile temel CRUD işlemleri tamamlandı

---
+++ ApiProjectCamp.WebApi/Controllers/CategoriesController.cs
@@ -0,0 +1,59 @@
+﻿using ApiProjectCamp.WebApi.Context;
+using ApiProjectCamp.WebApi.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProjectCamp.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ApiContext _context;
+
+        public CategoriesController(ApiContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult CategoryList()
+        {
+            var values = _context.Categories.ToList();
+            return Ok(values);
+        }
+
+
+        [HttpPost]
+        public IActionResult CreateCategory(Category category)
+        {
+            _context.Categories.Add(category);
+            _context.SaveChanges();
+            return Ok("Kategori Ekleme İşlemi Başarılı");
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteCategory(int id)
+        {
+            var value = _context.Categories.Find(id);
+            _context.Categories.Remove(value);
+            _context.SaveChanges();
+            return Ok("Kategori Silme İşlemi Gerçekleşti");
+        }
+
+        [HttpGet("GetCategory")]
+        public IActionResult GetCategory(int id)
+        {
+            var value = _context.Categories.Find(id);
+            return Ok(value);
+        }
+
+        [HttpPut]
+        public IActionResult UpdateCategory(Category category)
+        {
+            _context.Categories.Update(category);
+            _context.SaveChanges();
+            return Ok("Kategori Güncelleme İşlemi Başarılı");
+        }
+    }
+}
