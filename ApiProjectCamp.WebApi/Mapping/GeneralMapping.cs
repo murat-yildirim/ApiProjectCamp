3c7cf3af8a3c28d5e16cce81fbfca815a0175991
Murat Yıldırım <30393559+murat-yildirim@users.noreply.github.com>
2025-02-23 15:12:52 +0300

    11.Ders Sonu Feature ve Message için Dtos yazıldı AutoMap leme işlemi için paket yüklendi GenerelMapping içerisine metotlar yazıldı

---
+++ ApiProjectCamp.WebApi/Mapping/GeneralMapping.cs
@@ -0,0 +1,25 @@
+﻿using ApiProjectCamp.WebApi.Dtos.FeatureDtos;
+using ApiProjectCamp.WebApi.Dtos.MessageDtos;
+using ApiProjectCamp.WebApi.Entities;
+using AutoMapper;
+
+namespace ApiProjectCamp.WebApi.Mapping
+{
+    public class GeneralMapping : Profile
+    {
+        public GeneralMapping()
+        {
+            CreateMap<Feature,ResultFeatureDto>().ReverseMap(); 
+            CreateMap<Feature,CreateFeatureDto>().ReverseMap(); 
+            CreateMap<Feature,UpdateFeatureDto>().ReverseMap(); 
+            CreateMap<Feature,GetByIdFeatureDto>().ReverseMap(); 
+
+            CreateMap<Message,ResultMessageDto>().ReverseMap();
+            CreateMap<Message, CreateMessageDto>().ReverseMap();
+            CreateMap<Message, UpdateMessageDto>().ReverseMap();
+            CreateMap<Message, GetByIdMessageDto>().ReverseMap();
+
+
+        }
+    }
+}
