8c05ff6156f44a5c49e37cd064dc08a45cb2dbd2
Murat Yıldırım <30393559+murat-yildirim@users.noreply.github.com>
2025-04-22 22:23:11 +0300

    25 ve 26.Ders sonu

---
+++ ApiProjeKampi.WebUI/ViewComponents/DefaultMenuViewComponents/_DefaultMenuCategoryComponentPartial.cs
@@ -0,0 +1,34 @@
+﻿using ApiProjeKampi.WebUI.Dtos.CategoryDtos;
+using ApiProjeKampi.WebUI.Dtos.ServiceDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace ApiProjeKampi.WebUI.ViewComponents.DefaultMenuViewComponents
+{
+    public class _DefaultMenuCategoryComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DefaultMenuCategoryComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+
+            var client = _httpClientFactory.CreateClient();
+            //istekte bulunucağımız adresi belirliyoruz
+            var responseMessage = await client.GetAsync("https://localhost:7215/api/Categories/");
+            if (responseMessage.IsSuccessStatusCode)//200'lü durum kodlarını temsil eder
+            {
+                //boş bir değişken tanımladık responsemessage'den gelen içeriği string olarak oku
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+
+                var values = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
