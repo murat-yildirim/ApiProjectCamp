b274a806c1f000b96b28842433cd18e94f8588bb
Murat Yıldırım <30393559+murat-yildirim@users.noreply.github.com>
2025-05-06 00:13:25 +0300

    27.Ders sonu

---
+++ ApiProjeKampi.WebUI/ViewComponents/_TestimonialDefaultComponentPartial.cs
@@ -0,0 +1,35 @@
+﻿using ApiProjeKampi.WebUI.Dtos.ProductDtos;
+using ApiProjeKampi.WebUI.Dtos.TestimonialDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace ApiProjeKampi.WebUI.ViewComponents
+{
+    public class _TestimonialDefaultComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _TestimonialDefaultComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+
+            var client = _httpClientFactory.CreateClient();
+            //istekte bulunucağımız adresi belirliyoruz
+            var responseMessage = await client.GetAsync("https://localhost:7215/api/Testimonials/");
+            if (responseMessage.IsSuccessStatusCode)//200'lü durum kodlarını temsil eder
+            {
+                //boş bir değişken tanımladık responsemessage'den gelen içeriği string olarak oku
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+
+                var values = JsonConvert.DeserializeObject<List<ResultTestimonialDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
---
+++ ApiProjectCamp.WebApi/Controllers/TestimonialsController.cs
@@ -0,0 +1,59 @@
+﻿using ApiProjectCamp.WebApi.Context;
+using ApiProjectCamp.WebApi.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProjectCamp.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestimonialsController : ControllerBase
+    {
+        private readonly ApiContext _context;
+
+        public TestimonialsController(ApiContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult TestimonialList()
+        {
+            var values = _context.Testimonials.ToList();
+            return Ok(values);
+        }
+
+
+        [HttpPost]
+        public IActionResult CreateTestimonial(Testimonial Testimonial)
+        {
+            _context.Testimonials.Add(Testimonial);
+            _context.SaveChanges();
+            return Ok("Referans Ekleme İşlemi Başarılı");
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteTestimonial(int id)
+        {
+            var value = _context.Testimonials.Find(id);
+            _context.Testimonials.Remove(value);
+            _context.SaveChanges();
+            return Ok("Referans Silme İşlemi Gerçekleşti");
+        }
+
+        [HttpGet("GetTestimonial")]
+        public IActionResult GetTestimonial(int id)
+        {
+            var value = _context.Testimonials.Find(id);
+            return Ok(value);
+        }
+
+        [HttpPut]
+        public IActionResult UpdateTestimonial(Testimonial Testimonial)
+        {
+            _context.Testimonials.Update(Testimonial);
+            _context.SaveChanges();
+            return Ok("Referans Güncelleme İşlemi Başarılı");
+        }
+    }
+}
