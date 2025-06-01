aa4d4804804268f2e28c26c36cbd0ebf06aa70d4
Murat Yıldırım <30393559+murat-yildirim@users.noreply.github.com>
2025-06-01 22:26:33 +0300

    30.ders sonu chef api ve footer alanı tamamlandı

---
+++ ApiProjeKampi.WebUI/ViewComponents/_ChefDefaultComponentPartial.cs
@@ -0,0 +1,33 @@
+﻿using ApiProjeKampi.WebUI.Dtos.ChefDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace ApiProjeKampi.WebUI.ViewComponents
+{
+    public class _ChefDefaultComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _ChefDefaultComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+
+            var client = _httpClientFactory.CreateClient();
+            //istekte bulunucağımız adresi belirliyoruz
+            var responseMessage = await client.GetAsync("https://localhost:7215/api/Chefs/");
+            if (responseMessage.IsSuccessStatusCode)//200'lü durum kodlarını temsil eder
+            {
+                //boş bir değişken tanımladık responsemessage'den gelen içeriği string olarak oku
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+
+                var values = JsonConvert.DeserializeObject<List<ResultChefDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
