ad6cec156963a0ab4875b39b50e7120744aced0f
Murat Yıldırım <30393559+murat-yildirim@users.noreply.github.com>
2025-06-01 21:24:40 +0300

    28 ve 29.Ders sonu Events api ve controller tamamlandı

---
+++ ApiProjeKampi.WebUI/ViewComponents/_EventDefaultComponentPartial.cs
@@ -0,0 +1,33 @@
+﻿using ApiProjeKampi.WebUI.Dtos.EventDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace ApiProjeKampi.WebUI.ViewComponents
+{
+    public class _EventDefaultComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _EventDefaultComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+
+            var client = _httpClientFactory.CreateClient();
+            //istekte bulunucağımız adresi belirliyoruz
+            var responseMessage = await client.GetAsync("https://localhost:7215/api/YummyEvents/");
+            if (responseMessage.IsSuccessStatusCode)//200'lü durum kodlarını temsil eder
+            {
+                //boş bir değişken tanımladık responsemessage'den gelen içeriği string olarak oku
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+
+                var values = JsonConvert.DeserializeObject<List<ResultEventDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
