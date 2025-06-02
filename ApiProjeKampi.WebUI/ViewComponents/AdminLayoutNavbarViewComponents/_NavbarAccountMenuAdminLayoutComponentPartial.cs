8f49e10296143ac8c0ce92e5fefecb5387d51171
Murat Yıldırım <30393559+murat-yildirim@users.noreply.github.com>
2025-06-03 00:27:39 +0300

    35 ve 36.Ders Sonu notification listelenmesi ve navbar işlemlerinin tamamlanması

---
+++ ApiProjeKampi.WebUI/ViewComponents/AdminLayoutNavbarViewComponents/_NavbarAccountMenuAdminLayoutComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProjeKampi.WebUI.ViewComponents.AdminLayoutNavbarViewComponents
+{
+    public class _NavbarAccountMenuAdminLayoutComponentPartial : ViewComponent
+    {
+     
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ ApiProjeKampi.WebUI/ViewComponents/AdminLayoutNavbarViewComponents/_NavbarNotificationAdminLayoutComponentPartial.cs
@@ -0,0 +1,35 @@
+﻿using ApiProjeKampi.WebUI.Dtos.MessageDtos;
+using ApiProjeKampi.WebUI.Dtos.NotificationDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace ApiProjeKampi.WebUI.ViewComponents.AdminLayoutNavbarViewComponents
+{
+    public class _NavbarNotificationAdminLayoutComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _NavbarNotificationAdminLayoutComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+
+            var client = _httpClientFactory.CreateClient();
+            //istekte bulunucağımız adresi belirliyoruz
+            var responseMessage = await client.GetAsync("https://localhost:7215/api/Notifications");
+            if (responseMessage.IsSuccessStatusCode)//200'lü durum kodlarını temsil eder
+            {
+                //boş bir değişken tanımladık responsemessage'den gelen içeriği string olarak oku
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+
+                var values = JsonConvert.DeserializeObject<List<ResultNotificationDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+   
+}
