502049d9c517a71cd6a8130685577b733ef879cc
Murat Yıldırım <30393559+murat-yildirim@users.noreply.github.com>
2025-06-02 22:26:09 +0300

    32.Ders sonu admin layout düzenleme işlemleri

---
+++ ApiProjeKampi.WebUI/ViewComponents/AdminLayoutViewComponents/_NavbarAdminLayoutComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProjeKampi.WebUI.ViewComponents.AdminLayoutViewComponents
+{
+    public class _NavbarAdminLayoutComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+    
+}
